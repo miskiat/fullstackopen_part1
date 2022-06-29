5c1f6751c66b35976372c67a8ab749ce88d4d31d
Miskiat Badmus <badmusmiskiat@gmail.com>
2022-06-29 07:06:51 +0100

    1.1: course information, step1

---
+++ courseinfo/src/App.js
@@ -0,0 +1,63 @@
+const Header = ({ course }) => {
+  return (
+    <div>
+      <h1>{course}</h1>
+    </div>
+  );
+};
+const Content = ({
+  part1,
+  exercises1,
+  part2,
+  exercises2,
+  part3,
+  exercises3,
+}) => {
+  return (
+    <div>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+    </div>
+  );
+};
+const Total = ({ exercises1, exercises2, exercises3 }) => {
+  return (
+    <div>
+      <p>Total {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} exercise1={exercises1} />
+      <Content part2={part2} exercise2={exercises2} />
+      <Content part3={part3} exercise3={exercises3} />
+
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+    </div>
+  );
+};
+
+export default App;
