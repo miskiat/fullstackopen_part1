ea07ac715cfb94ae93b08495a0e814506259ded6
Miskiat Badmus <badmusmiskiat@gmail.com>
2022-06-30 18:15:38 +0100

    1.7: unicafe step2

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -17,6 +17,9 @@ const App = () => {
     setBad(bad + 1);
   }
 
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = `${(good / all) * 100}%`;
   return (
     <div>
       <h1>give feedback</h1>
@@ -27,6 +30,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}</p>
     </div>
   );
 };
