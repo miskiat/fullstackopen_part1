595d783ba01a85a2287cec52f44918aa92693a35
Miskiat Badmus <badmusmiskiat@gmail.com>
2022-07-03 23:11:10 +0100

    1.12*: anecdotes step1

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,34 +1,27 @@
 import React, { useState } from "react";
-import Statistics from "./Statistics";
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+  ];
 
-export default function App() {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [selected, setSelected] = useState(0);
+  const generateRandom = () => {
+    let random = Math.floor(Math.random() * anecdotes.length);
+    setSelected(random);
+  };
 
-  const all = good + neutral + bad;
-  const average = (good - bad) / all;
-  const positive = `${(good / all) * 100}%`;
   return (
     <div>
-      <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h1>statistics</h1>
-      {all ? (
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          sum={all}
-          average={average}
-          positive={positive}
-        />
-      ) : (
-        "No feedback given"
-      )}
-      ; ;
+      <p>{anecdotes[selected]}</p>
+      <button onClick={generateRandom}>next anecdote</button>
     </div>
   );
-}
+};
+
+export default App;
